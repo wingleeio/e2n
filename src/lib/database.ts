68d008cc64f6a98dac5c3cf2447a4853c0901d02
Wing Lee <contact@winglee.io>
2024-06-01 13:43:11 -0700

    Update database.ts

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,6 +2,4 @@ import createClient from "edgedb";
 
 export const client = createClient({
     tlsSecurity: process.env.NODE_ENV === "development" ? "insecure" : undefined,
-}).withConfig({
-    allow_user_specified_id: true,
 });
